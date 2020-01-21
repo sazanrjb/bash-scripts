a65e3f9747578962ca4a7c8a8ed86a0aa3a419ed
Sajan Rajbhandari <sazanrjb@gmail.com>
2020-01-21 21:31:46 +0545

    Remove comments

--- add-users/addUsers.sh
+++ add-users/addUsers.sh
@@ -5,8 +5,8 @@ do
 
 user=($line)
 password="$(echo -n "${user[0]}" | sha256sum | head -c 15)"
-#sudo groupadd -f ${user[1]}
-#sudo useradd  -d ${user[2]} -g ${user[1]} -c ${user[3]} ${user[0]}
-#echo ${user[0]}":"$password|sudo chpasswd
+sudo groupadd -f ${user[1]}
+sudo useradd  -d ${user[2]} -g ${user[1]} -c ${user[3]} ${user[0]}
+echo ${user[0]}":"$password|sudo chpasswd
 echo "Created at " $(date) ": " ${user[0]} ${user[1]} ${user[2]} ${user[3]} $password >> usersOutput.txt
 done
